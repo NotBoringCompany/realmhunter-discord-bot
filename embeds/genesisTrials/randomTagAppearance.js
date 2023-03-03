4bfae16510919cba91047b742865ac7095d4b9a1
suwandre <suwandresukijat@gmail.com>
2023-03-03 12:46:27 +0100

    updated image for stellas cookies

--- embeds/genesisTrials/randomTagAppearance.js
+++ embeds/genesisTrials/randomTagAppearance.js
@@ -5,7 +5,8 @@ const randomTagAppearanceEmbed = new EmbedBuilder()
     .setTitle('Stella\'s Cookies up for grabs!')
     .setDescription('Stella has dropped 5 cookies while making a huge batch! Quick, collect them now by typing __**!hunt collectCookies**__ in the chat. \n\
     First candidate to collect them will get all 5 cookies.',
-    );
+    )
+    .setImage('https://i.imgur.com/HkPn7je.png');
 
 module.exports = {
     randomTagAppearanceEmbed,
