09d89d7a65b3da95bfb5774f60a6a8da80945597
suwandre <suwandresukijat@gmail.com>
2023-03-21 15:10:27 +0100

    fixed manuallyrewardtags

--- commands/genesisTrials/manualRewarding.js
+++ commands/genesisTrials/manualRewarding.js
@@ -4,11 +4,16 @@ const manuallyRewardTags = async (message) => {
     try {
         const [hunt, manuallyRewardTags, toGeneralChat, userId, tagsToGive, ...reason] = message.content.split(' ');
 
-        if (toGeneralChat !== 'toGeneralChat' || toGeneralChat !== 'notToGeneralChat') {
-            return await message.reply('Please specify if you want to send the message to the general chat or not.');
-        }
+        console.log(toGeneralChat);
 
-        return await manuallyRewardTagsLogic(userId, tagsToGive, reason.join(' '));
+        if (toGeneralChat.toLowerCase() === 'togeneralchat' || toGeneralChat.toLowerCase() === 'nottogeneralchat') {
+            return await manuallyRewardTagsLogic(userId, tagsToGive, reason.join(' '));
+        } else {
+            return {
+                status: 'error',
+                message: 'Please specify whether to send the message to general chat or not.',
+            };
+        }
     } catch (err) {
         console.log({
             errorFrom: 'manuallyRewardTags',
--- server.js
+++ server.js
@@ -193,9 +193,9 @@ client.on('messageCreate', async (message) => {
                 return await message.channel.send(rewardMessage);
             // otherwise, send it to #general-chat.
             } else {
-                if (message.content.contains('toGeneralChat')) {
+                if (message.content.toLowerCase().contains('togeneralchat')) {
                     return await client.channels.cache.get(process.env.GENERAL_CHAT_CHANNELID).send(rewardMessage);
-                } else if (message.content.contains('notToGeneralChat')) {
+                } else if (message.content.toLowerCase().contains('nottogeneralchat')) {
                     return await message.channel.send(rewardMessage);
                 }
             }
